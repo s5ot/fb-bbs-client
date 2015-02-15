e49abf3ed51650446d1eec84738555eeda84dfd7
sogo.ohta <sogo.ohta@gmail.com>
2015-02-15 16:48:55 +0900

    Fix

--- build.config.js
+++ build.config.js
@@ -62,12 +62,14 @@ module.exports = {
    */
   vendor_files: {
     js: [
+      'vendor/jquery/dist/jquery.js',
       'vendor/angular/angular.js',
       'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
       'vendor/angular-ui-utils/modules/route/route.js',
-      'vendor/ng-facebook/ngFacebook.js'
+      'vendor/ng-facebook/ngFacebook.js',
+      'vendor/ngInfiniteScroll/build/ng-infinite-scroll.js'
     ],
     css: [
     ],
--- src/app/home/home.js
+++ src/app/home/home.js
@@ -34,50 +34,6 @@ angular.module('ngBoilerplate.home', [
     });
   };
 
-  $scope.confirmPost = function(topicId, postContent) {
-    $scope.topicId = topicId;
-    $scope.postContent = postContent;
-    var modalInstance = $modal.open({
-      templateUrl: 'home/confirm_post.tpl.html',
-      controller: 'ConfirmPostModalCtrl',
-      size: 'lg',
-      resolve: {
-        postContent: function () {
-          return $scope.postContent;
-        },
-        photo: function () {
-          return $scope.choosedPhoto;
-        }
-      }
-    });
-
-    modalInstance.result.then(function (photo) {
-      console.log('ok');
-      var imgUrl = photo ? photo.picture : '';
-      var imgId = photo ? photo.id : '';
-
-      $http.post('http://localhost:3000/posts.json', {
-        post: {
-        topic_id: $scope.topicId,
-        content: $scope.postContent,
-        user_id: $scope.userId,
-        img_url: imgUrl,
-        img_id: imgId
-        }
-      }).
-      success(function(data, status, headers, config) {
-        $scope.fetchTopics();
-        $scope.postContent = null;
-        $scope.choosedPhoto = null;
-      }).
-      error(function(data, status, headers, config) {
-        console.error(data);
-      });
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-
   $scope.createTopic = function(topicTitle, postContent) {
     var imgUrl = $scope.choosedPhoto ? $scope.choosedPhoto.picture : '';
     var imgId = $scope.choosedPhoto ? $scope.choosedPhoto.id : '';
@@ -145,7 +101,25 @@ angular.module('ngBoilerplate.home', [
   };
 })
 
-.controller('TopicCtrl', function HomeController($scope, $http, $modal, $log, $facebook) {
+.controller('TopicCtrl', function HomeController($scope, $http, $modal, $log, $facebook, Reddit, $stateParams, $state) {
+
+  if ($stateParams.topicId) {
+    $scope.reddit = new Reddit($stateParams.topicId);
+    $scope.topicId = $stateParams.topicId;
+  }
+
+  $scope.fetchPosts = function() {
+    console.log('fetchPosts');
+
+    $http.get('http://localhost:3000/topics/' + $scope.topicId + '.json')
+    .success(function(data) {
+      $scope.topic = data;
+    })
+    .error(function(data) {
+      console.error(data);
+    });
+  };
+
   $scope.openAlbum = function(size) {
     $facebook.api('/me/albums').then(
      function(data) {
@@ -211,6 +185,7 @@ angular.module('ngBoilerplate.home', [
         }
         $scope.postContent = null;
         $scope.choosedPhoto = null;
+        $state.go('home');
       }).
       error(function(data, status, headers, config) {
         console.error(data);
@@ -245,4 +220,67 @@ angular.module('ngBoilerplate.home', [
       console.error(data);
     });
   };
+
+  $scope.fetchPosts = function() {
+    /*
+    console.log('fetchPosts');
+    $http.get('http://localhost:3000/topics/' + $stateParams.topicId + '.json')
+    .success(function(data) {
+      $scope.topic = data;
+
+      if (!$scope.page) {
+        $scope.page = 1;
+      } else {
+        $scope.page += 1;
+      }
+      if (!$scope.topic.posts) {
+        $scope.topic.posts =[];
+      }
+
+      $http.get('http://localhost:3000/posts.json?' + 'topic_id=' + $stateParams.topicId + '&page=' + $scope.page)
+      .success(function(data) {
+        angular.forEach(data, function(p) {
+          $scope.topic.posts.push(p);
+        });
+      })
+      .error(function(data) {
+        console.error(data);
+      });
+    })
+    .error(function(data) {
+      console.error(data);
+    });
+    */
+  };
+})
+.factory('Reddit', function($http) {
+  var Reddit = function(topicId) {
+    this.topicId = topicId;
+    this.page = 1;
+    this.items = [];
+    this.busy = false;
+    this.after = '';
+  };
+
+  Reddit.prototype.nextPage = function(topicId) {
+    if (this.busy) {
+      return;
+    }
+    this.busy = true;
+
+    $http.get('http://localhost:3000/posts.json?' + 'topic_id=' + this.topicId + '&page=' + this.page)
+    .success(function(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        this.items.push(data[i]);
+      }
+      this.busy = false;
+      this.page += 1;
+    }.bind(this))
+    .error(function(data) {
+      console.error(data);
+    });
+  };
+
+  return Reddit;
 });
